13e6f7cc7c3b9f8063831bc68bb7f81586496df5
ahuva.c <acheifet@student.touro.edu>
2021-12-27 21:08:30 -0500

    initial commit

---
+++ src/main/java/SettlersApplication.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SettlersApplication extends Application{
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/settlers_application.fxml"));
+        Scene scene = new Scene(root, 500, 250);
+        stage.setTitle("Settlers of Catan");
+        stage.setScene(scene);
+        stage.show();
+    }
+        public static void main(String[] args) {
+            launch(args);
+        }
+    }
+
