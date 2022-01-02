ffbe8fb1fd8a1e674b6e76f8c139be380598ac68
Nina Bildirici <nrb722@gmail.com>
2022-01-02 16:22:35 -0500

    Added a few classes

---
+++ src/main/java/Board.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+
+public class Board {
+    Hex[] board = new Hex[16];
+
+}
---
+++ src/main/java/Hex.java
@@ -0,0 +1,10 @@
+public class Hex {
+    ResourceType type;
+    int position;
+
+    public Hex(ResourceType type, int position) {
+        this.type = type;
+        this.position = position;
+    }
+
+}
