ebf3722074fab7bdee909239ace59666089a32cb
ahuva.c <acheifet@student.touro.edu>
2022-01-02 23:07:33 -0500

    working on board

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,6 +1,40 @@
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
 
 public class Board {
-    Hex[] board = new Hex[16];
+    //Hex[] board = new Hex[16];
+    List<Hex> board = new ArrayList<>();
+
+    public Board(){
+        for (int i = 0 ; i < 3; i++){
+            board.add( new Hex(ResourceType.ORE));
+        }
+
+        for (int i = 4 ; i < 7; i++){
+            board.add( new Hex(ResourceType.BRICK));
+        }
+
+        for (int i = 7 ; i < 10; i++){
+            board.add( new Hex(ResourceType.DESERT));
+        }
+
+        for (int i = 10 ; i < 14; i++){
+            board.add( new Hex(ResourceType.GRAIN));
+        }
+
+        for (int i = 14 ; i < 18; i++){
+            board.add( new Hex(ResourceType.LUMBER));
+        }
+
+        for (int i = 18 ; i < 22; i++){
+            board.add( new Hex(ResourceType.WOOL));
+        }
+
+        board.add( new Hex(ResourceType.ROBBER));
+
+        Collections.shuffle(board);
+    }
+
 
 }
--- src/main/java/Hex.java
+++ src/main/java/Hex.java
@@ -1,10 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hex {
     ResourceType type;
+    List<Hex> neighbors;
     int position;
 
-    public Hex(ResourceType type, int position) {
+
+    public Hex(ResourceType type) {
         this.type = type;
-        this.position = position;
+        this.neighbors = new ArrayList();
+//        this.position = position;
+    }
+
+
+    public void connect(){
+
     }
 
 }
