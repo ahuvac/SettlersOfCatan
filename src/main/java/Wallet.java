8aa565dfed58217e55344262199c78d2d5288d95
lwolfish <58049823+lwolfish@users.noreply.github.com>
2022-01-11 14:29:01 -0500

    Added methods to play development cards

--- src/main/java/Wallet.java
+++ src/main/java/Wallet.java
@@ -12,6 +12,18 @@ public class Wallet {
         cards.put(ResourceType.WOOL, 0);
     }
 
+    public boolean isEmpty()
+    {
+        for (ResourceType type : ResourceType.values()) {
+            if (type == ResourceType.DESERT) continue;
+            if(hasCard(type, 1))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public boolean hasCard(ResourceType type, int number)
     {
         int numberOfCards = cards.get(type);
@@ -36,6 +48,21 @@ public class Wallet {
     public void addCard(ResourceType card, int number) {
         int amount = cards.get(card);
         cards.put(card, amount+=number);
+    }
+
+    public ResourceType getRandomCard()
+    {
+        if (isEmpty())
+        {
+            return null;
+        }
+        while (true) {
+            ResourceType type = ResourceType.values()[new Random().nextInt(ResourceType.values().length)];
+            if (type != ResourceType.DESERT && hasCard(type, 1)) {
+                useCard(type);
+                return type;
+            }
+        }
     }
 
 }
