ede04d902d1dae7845b0deefa34e36388a13739d
lwolfish <58049823+lwolfish@users.noreply.github.com>
2022-01-10 21:20:07 -0500

    fixed bug in player

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -83,30 +83,16 @@ public class Player {
             settlements--;
     }
 
-    public void buildCity(){
+    public void buildCity() {
         useCard(ResourceType.ORE);
         useCard(ResourceType.ORE);
         useCard(ResourceType.ORE);
         useCard(ResourceType.GRAIN);
         useCard(ResourceType.GRAIN);
-      
-            score+=2;
-            cities--;
-            settlements++;
 
-
-    public boolean buyDevelopmentCard(){
-        Map<ResourceType, Integer> cardList = new HashMap<>();
-        cardList.put(ResourceType.GRAIN, 1);
-        cardList.put(ResourceType.ORE, 1);
-        cardList.put(ResourceType.WOOL, 1);
-        if(wallet.useCards(cardList)) {
-            //developmentCards.add(new DevelopmentCard());
-            return true;
-        }
-        else {
-            return false;
-        }
+        score += 2;
+        cities--;
+        settlements++;
     }
 
 
