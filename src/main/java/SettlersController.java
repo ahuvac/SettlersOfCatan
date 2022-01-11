efa66f839c470098675d3ec8057b88ec8028ce9e
Nina Bildirici <nrb722@gmail.com>
2022-01-11 12:27:53 -0500

    Fixed Board

--- src/main/java/SettlersController.java
+++ src/main/java/SettlersController.java
@@ -1,7 +1,38 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 
 public class SettlersController {
+
+    public void Player1GrainCardOnClick(MouseEvent mouseEvent) {
+    }
+
+    public void Player1LumberCardOnClick(MouseEvent mouseEvent) {
+    }
+
+    public void Player1WoolCardOnClick(MouseEvent mouseEvent) {
+    }
+
+    public void Player1OreCardOnClick(MouseEvent mouseEvent) {
+    }
+
+    public void Player1BrickCardOnClick(MouseEvent mouseEvent) {
+    }
+
+    public void Player2GrainCardOnClick(MouseEvent mouseEvent) {
+    }
+
+    public void Player2LumberCardOnClick(MouseEvent mouseEvent) {
+    }
+
+    public void Player2WoolCardOnClick(MouseEvent mouseEvent) {
+    }
+
+    public void Player2OreCardOnClick(MouseEvent mouseEvent) {
+    }
+
+    public void Player2BrickCardOnClick(MouseEvent mouseEvent) {
+    }
 }
