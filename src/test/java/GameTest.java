4dac01314de2966b3d9dd75aa42e24cfc132fa21
Nina Bildirici <nrb722@gmail.com>
2022-01-17 14:12:41 -0500

    added button

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -4,20 +4,40 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
 
+    //TODO Mocito to create mock dev card to see if it preforms correctly for each dev card
     @Test
     void buyDevelopmentCard() {
+
+        Game game = new Game();
+
+        Player player = game.getCurrentPlayer();
+
+        player.addCard(ResourceType.WOOL,1);
+        player.addCard(ResourceType.GRAIN,1);
+        player.addCard(ResourceType.ORE,1);
+
+        DevelopmentCard card = game.buyDevelopmentCard();
+
+        assertNotNull(card);
     }
 
+    //TODO write test
     @Test
     void gameBeginning() {
     }
 
+    //TODO
     @Test
     void chooseSettlementLocation() {
     }
 
+    //TODO
     @Test
     void chooseRoadLocation() {
+    }
+
+    @Test
+    void addToPlayerOrder(){
     }
 
     @Test
