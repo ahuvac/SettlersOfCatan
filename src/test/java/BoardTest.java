9e4428713e3a033a0fc8ca23e138702e280edfb1
ahuva.c <acheifet@student.touro.edu>
2022-01-21 13:04:03 -0500

    test

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -7,12 +7,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
 
-    @Test
-     void getLongestPath(){
-        Board board = new Board();
-        int maxPath = board.getLongestPath(new Player(Color.BLUE), board.edges[2][3]);
-        assertEquals(2, maxPath);
-    }
+//    @Test
+//     void getLongestPath(){
+//        Board board = new Board();
+//        int maxPath = board.getLongestPath(new Player(Color.BLUE), board.edges[2][3]);
+//        assertEquals(2, maxPath);
+//    }
 
 
 }
